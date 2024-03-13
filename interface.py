86e7a9a286ad34815d23a8d10ba7d3e4fad87057
Exteal <rh.4@laposte.net>
2024-03-13 23:51:48 +0100

    Add basic eye tracking

--- interface.py
+++ interface.py
@@ -1,3 +1,4 @@
+import csv
 import sys
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
@@ -5,14 +6,24 @@ from PyQt6.QtWebEngineWidgets import *
 from PyQt6.QtGui import *
 from menu import Menu
 from recommand_lib import recommand
+from numpy import zeros, uint8
+
+from GazeTracking.gaze_tracking import GazeTracking
+import cv2
+
 
 class Interface(QMainWindow):
-    def __init__(self):
+    def __init__(self, log_file):
         super().__init__()
-
+        self.log_file = log_file
         self.initUI()
 
     def initUI(self):
+
+        self.setMouseTracking(True)
+        
+
+
         # Créer le widget principal
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
@@ -69,11 +80,11 @@ class Interface(QMainWindow):
         right_layout.addLayout(spacing, 1)
         
         # Créer un widget QWebView pour l'accès au web
-        left_layout = QVBoxLayout()
+        #left_layout = QVBoxLayout()
         # text pour le titre
-        title = QLabel("Recherche Web")
-        title.setAlignment(Qt.AlignmentFlag.AlignTop)
-        title.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        #title = QLabel("Recherche Web")
+       # title.setAlignment(Qt.AlignmentFlag.AlignTop)
+        #title.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
         
         # ajouter une case de recherche
         #search_bar = QLineEdit(self)
@@ -83,18 +94,18 @@ class Interface(QMainWindow):
         # ajouter les widgets au layout
         #left_layout.addWidget(search_bar)
 
-        web_view = QWebEngineView(self)
-        web_view.setUrl(QUrl("https://google.com"))
-        web_view.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+       # web_view = QWebEngineView(self)
+       # web_view.setUrl(QUrl("https://google.com"))
+       # web_view.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
 
-        left_layout.addWidget(title)
-        left_layout.addWidget(web_view)
+       # left_layout.addWidget(title)
+        #left_layout.addWidget(web_view)
 
         # Ajouter les layouts
     
-        main_layout.addLayout(left_layout, 33)
-        main_layout.addLayout(center_layout, 33)
-        main_layout.addLayout(right_layout, 33)
+       # main_layout.addLayout(left_layout, 33)
+        main_layout.addLayout(center_layout, 50)
+        main_layout.addLayout(right_layout, 50)
 
         # Créer le widget central avec le layout principal
         central_widget.setLayout(main_layout)
@@ -131,11 +142,81 @@ class Interface(QMainWindow):
         # Effacer la sortie du terminal
         self.terminal_output.clear()
 
+
+    def mouseMoveEvent(self, event):
+        print("Mouse Pos: ", event.pos())
+        super().mouseMoveEvent(event)
+        
+    def mousePressEvent(self, event):
+        text = str(str(event.pos().x()) + " " + str(event.pos().y()))
+        print("XXXXXXXXXXXXXXX")
+
+        
+        self.log_file.write(str(event.pos().x()))
+        self.log_file.write("-")
+        
+        self.log_file.write(str(event.pos().y()))
+
+        super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        print("Mouse Released at: ", event.pos())
+        super().mouseReleaseEvent(event)
+    
+       
+
 def main(args):
-    app = QApplication(args)
-    interface = Interface()
-    interface.show()
-    sys.exit(app.exec())
+    with open('log.txt','a+') as log_file:
+       # it = csv.DictWriter(log_file, ["click"])
+       # it.writeheader()
+       # it.writerows()
+
+        app = QApplication(args)
+        interface = Interface(log_file)
+        interface.show()
+
+        gaze = GazeTracking()
+        webcam = cv2.VideoCapture(0)
+        while True:
+
+            height = 400
+            width = 500
+
+            image  = zeros((height,width,3), uint8)
+                # We get a new frame from the webcam
+            _, frame = webcam.read()
+
+            # We send this frame to GazeTracking to analyze it
+            gaze.refresh(frame)
+
+            frame = gaze.annotated_frame()
+            text = ""
+
+            #if gaze.is_blinking():
+            #    text = "Blinking"
+            if gaze.is_right():
+                text = "Looking right"
+            elif gaze.is_left():
+                text = "Looking left"
+            elif gaze.is_center():
+                text = "Looking center"
+
+            cv2.putText(image, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
+            left_pupil = gaze.pupil_left_coords()
+            right_pupil = gaze.pupil_right_coords()
+            cv2.putText(image, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+            cv2.putText(image, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+            if (gaze.horizontal_ratio() is not None):
+                cv2.putText(image, "Direction : " + str(gaze.horizontal_ratio()), (90, 190), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+            cv2.imshow("Demo", image)
+
+            if cv2.waitKey(1) == 27:
+                break
+        
+        webcam.release()
+        cv2.destroyAllWindows()
+        sys.exit(app.exec())
     
     
 if __name__ == '__main__':
